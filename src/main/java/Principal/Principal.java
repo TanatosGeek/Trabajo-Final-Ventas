26c1dcca96f995a1f9fc5166ba79b0b153c3cbe0
Manuel Eduardo <124794591+TanatosGeek@users.noreply.github.com>
2023-03-01 21:26:50 -0600

    Probando el repositorio

--- src/main/java/Principal/Principal.java
+++ src/main/java/Principal/Principal.java
@@ -11,5 +11,6 @@ package Principal;
 public class Principal {
     public static void main(String args[]){
         System.out.println("Hola mundo");
+        System.out.println("Como estas mundo");
     }
 }
