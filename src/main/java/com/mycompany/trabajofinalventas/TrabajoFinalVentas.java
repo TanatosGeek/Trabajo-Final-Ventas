7fec67d794c8bc1e248e25b696139fe78283c95d
Manuel Eduardo <124794591+TanatosGeek@users.noreply.github.com>
2023-03-01 19:08:54 -0600

    Probando el repositorio

---
+++ src/main/java/com/mycompany/trabajofinalventas/TrabajoFinalVentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajofinalventas;
+
+/**
+ *
+ * @author mayka
+ */
+public class TrabajoFinalVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
